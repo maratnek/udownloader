7b5a8d4d01d019ffcb7f6b5c30ed2df161a2a1f7
Marat Zabirov <maratn566ek@gmail.com>
2021-07-22 00:09:39 +0300

    correct packate version

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const ffmpeg = require('fluent-ffmpeg')
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 let index = 4;
 {
-    // const stream = ytdl(`https://www.youtube.com/watch?v=imZ9D9HMftk&ab_channel=LongBeachFinest`, { filter: format => format.container === 'mp4' });
-    const stream = ytdl(`https://www.youtube.com/watch?v=imZ9D9HMftk&ab_channel=LongBeachFinest`)
+    const stream = ytdl(`https://www.youtube.com/watch?v=imZ9D9HMftk&ab_channel=LongBeachFinest`, { filter: format => format.container === 'mp4' });
+    // const stream = ytdl(`https://www.youtube.com/watch?v=imZ9D9HMftk&ab_channel=LongBeachFinest`)
     // .pipe(fs.createWriteStream('video.mp4'))
     // .pipe(ffmpeg);
     ffmpeg({ source: stream })
